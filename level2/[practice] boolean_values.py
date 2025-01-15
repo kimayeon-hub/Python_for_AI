d0f8d4dc3f77a9c15b545540f8d92599149dda82
kimayeon-hub <ayun4953@gmail.com>
2025-01-15 13:31:34 +0900

    boolean values practice

--- level2/[practice] boolean_values.py
+++ level2/[practice] boolean_values.py
@@ -3,6 +3,6 @@ test_list = []
 print(len(test_list) == 0)
 
 # 리스트에 들어 있는 원소의 개수가 입력 값보다 작은지 검사
-test_list = [1, 2, 3]
+test_list = [1, 2, 3, 4]
 threshold_input = int(input("Enter a threshold: "))
 print(len(test_list) < threshold_input)
