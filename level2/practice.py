a28678bbf4b1ca25e7cb2cf356b7b977368d4148
kimayeon-hub <ayun4953@gmail.com>
2025-01-15 12:10:37 +0900

    input function practice

--- level2/practice.py
+++ level2/practice.py
@@ -1,5 +1,8 @@
+# 학생 수 입력 받아, 학생 수 만큼 랜덤으로 점수 생성해서 리스트에 넣기
+
 import random
 
 n_students = int(input("Enter a number of students: "))
 scores = [random.randint(0,100) for _ in range(n_students)]
 print(f"{len(scores) = }")
+print(scores)
