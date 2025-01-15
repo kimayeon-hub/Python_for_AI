a109331d3b92af7e53681a442d9225784f2e11df
kimayeon-hub <ayun4953@gmail.com>
2025-01-15 14:13:30 +0900

    ReLU by if, else statement

--- level2/ReLU.py
+++ level2/ReLU.py
@@ -1,7 +1,7 @@
 # Rectified Linear Unit (ReLU)
 # 딥러닝에서 activation function으로 ReLU를 사용함
 
-# 값이 0보다 크면 그대로, 0보다 작으면 0 반환
+# 값이 0보다 크면 그대로, 0보다 작으면 0을 반환
 # if, else statement로 구현
 
 num = int(input("Enter a number: "))
