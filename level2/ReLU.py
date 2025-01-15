760c0d1aa41c346336fa481771a69114a82ffa1d
kimayeon-hub <ayun4953@gmail.com>
2025-01-15 16:20:12 +0900

    if-else statement & for loop practice

--- level2/ReLU.py
+++ level2/ReLU.py
@@ -2,11 +2,29 @@
 # 딥러닝에서 activation function으로 ReLU를 사용함
 
 # 값이 0보다 크면 그대로, 0보다 작으면 0을 반환
-# if, else statement로 구현
 
+
+# if, else statement로 구현
 num = int(input("Enter a number: "))
+
 if num > 0:
     relu = num
 else:
     relu = 0
 print(f"ReLU({num}) = {relu}")
+
+
+# for loop + if-else statement로 구현
+import random
+
+n_data = 10
+data = [random.randint(-10, 10) for _ in range(n_data)]
+print(f"{data = }")
+
+relu_values = []
+for d in data:
+    if d >= 0:
+        relu_values.append(d)
+    else:
+        relu_values.append(0)
+print(f"{relu_values = }")
