bffe71f284001c1c9e2fea7776d95a7877551096
kimayeon-hub <ayun4953@gmail.com>
2025-01-15 13:25:44 +0900

    input function practice

---
+++ level2/boolean_values.py
@@ -0,0 +1,24 @@
+# boolean value
+print(type(True))
+print(type(False))
+
+# and 연산
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+# or 연산
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+# 비교 연산의 결과로 boolean value가 나옴
+num1, num2 = 10, 5
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 == num2)
+print(num1 != num2)
