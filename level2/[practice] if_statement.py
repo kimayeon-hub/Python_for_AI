f450c7b604546f7c17d0ae2a9247c01f518f0c5b
kimayeon-hub <ayun4953@gmail.com>
2025-01-16 16:39:48 +0900

    list comprehension with if statement

--- level2/[practice] if_statement.py
+++ level2/[practice] if_statement.py
@@ -93,3 +93,18 @@ if IS_PRIME:
 else:
     print(f"{num} is not a prime number")
 
+
+# List Comprehension + If Statement
+import random
+
+n_students = 100
+threshold = 80
+
+scores = [random.randint(0, 100) for _ in range(n_students)]
+print(f"{scores = }\n")
+
+scores_pass = [score for score in scores if score >= threshold]
+scores_fail = [score for score in scores if score < threshold]
+print(f"{scores_pass = }")
+print(f"{scores_fail = }")
+
