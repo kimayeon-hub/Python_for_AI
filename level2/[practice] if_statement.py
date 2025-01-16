d3bc7b7b2676b6f4c56279de5e6208cbc03b7051
kimayeon-hub <ayun4953@gmail.com>
2025-01-16 16:10:20 +0900

    3n cycle

--- level2/[practice] if_statement.py
+++ level2/[practice] if_statement.py
@@ -14,3 +14,13 @@ if num > 0:
 else:
     abs_val = -num
 print(f"absolute value of {num} is {abs_val}")
+
+
+# 3n cycle을 구현
+for num in range(10):
+    if num % 3 == 0:
+        print(f"{num} is 3n")
+    elif num % 3 == 1:
+        print(f"{num} is 3n + 1")
+    else:
+        print(f"{num} is 3n + 2")
