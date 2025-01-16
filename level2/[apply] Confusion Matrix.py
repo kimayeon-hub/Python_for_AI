e93abac6454712376430c6d3868ddd6a6a99a25e
kimayeon-hub <ayun4953@gmail.com>
2025-01-16 15:24:53 +0900

    another method

---
+++ level2/[apply] Confusion Matrix.py
@@ -0,0 +1,19 @@
+import random
+
+n_data = 100
+
+labels = [random.randint(0, 1) for _ in range(n_data)]
+predictions = [random.randint(0, 1) for _ in range(n_data)]
+print(f"{labels = }")
+print(f"{predictions = }\n")
+
+n_TP, n_TN, n_FP, n_FN = 0, 0, 0, 0
+for label, pred in zip(labels, predictions):
+    if label == pred:
+        if pred == 0: n_TN += 1
+        else: n_TP += 1
+    else:
+        if pred == 0: n_FN += 1
+        else: n_FP += 1
+print(f"{n_TP = } | {n_FN = }")
+print(f"{n_FP = } | {n_TN = }")
