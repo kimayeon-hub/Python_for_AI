87eb3921f42f9099acd03650e579bfb1d26b3bfa
kimayeon-hub <ayun4953@gmail.com>
2025-01-16 17:35:14 +0900

    commit

---
+++ level2/[apply] sort in ascending order.py
@@ -0,0 +1,18 @@
+import random
+
+n_students = 10
+scores = [random.randint(0, 100) for _ in range(n_students)]
+print(f"{scores = }")
+
+scores_tmp = scores.copy()
+scores_sort = []
+
+while len(scores_tmp) != 0:
+    min_score, min_score_idx = scores_tmp[0], 0
+    for idx, score in enumerate(scores_tmp):
+        if score < min_score:
+            min_score = score
+            min_score_idx = idx
+    scores_sort.append(min_score)
+    scores_tmp.pop(min_score_idx)
+print(f"{scores_sort = }")
