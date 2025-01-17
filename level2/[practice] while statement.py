a17ec6132a9792547aea528b5d0dbbc83211e110
kimayeon-hub <ayun4953@gmail.com>
2025-01-17 17:30:40 +0900

    another method

--- level2/[practice] while statement.py
+++ level2/[practice] while statement.py
@@ -8,3 +8,24 @@ while cum_sum <= threshold_sum:
     i += 1
     print(f"{i} - {cum_sum}")
 
+
+# while statement + break
+i = 0
+while True:
+    i += 1
+    print(i)
+
+    if i == 10:
+        break
+
+
+# n을 넘는 최초의 누적합 2
+threshold_sum = int(input("Enter a number: "))
+
+cum_sum, i = 0, 0
+while True:
+    if cum_sum > threshold_sum:
+        break
+    cum_sum += i
+    i += 1
+    print(f"{i} - {cum_sum}")
