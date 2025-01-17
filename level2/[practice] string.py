d38b58f85a8b6e9e3bbf2fcf74b84143c4c2d1aa
kimayeon-hub <ayun4953@gmail.com>
2025-01-17 14:29:50 +0900

    string + for loop

--- level2/[practice] string.py
+++ level2/[practice] string.py
@@ -4,6 +4,10 @@ test_string = "abcdef"
 print(test_string[0])
 print(test_string[1])
 print(test_string[-1])
-print(test_string[-2])
+print(test_string[-2], '\n')
 
 
+# string + for loop
+test_string = "abcdef"
+for char in test_string:
+    print(char)
