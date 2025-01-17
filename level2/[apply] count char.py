cefe1ab2c4a0b545e615c47389857de3e03c472e
kimayeon-hub <ayun4953@gmail.com>
2025-01-17 14:36:53 +0900

    implement logic

--- level2/[apply] count char.py
+++ level2/[apply] count char.py
@@ -5,9 +5,10 @@ test_string = 'aabbbbccccd'
 
 count_char_dict = {}
 for ch in test_string:
-    if count_char_dict.get(ch) == None:
-        count_char_dict[ch] = 1
-    elif count_char_dict.get(ch) != None:
-        count_char_dict[ch] += 1
+    # if count_char_dict.get(ch) == None:
+    #     count_char_dict[ch] = 1
+    # elif count_char_dict.get(ch) != None:
+    #     count_char_dict[ch] += 1
+    count_char_dict[ch] = count_char_dict.get(ch, 0) + 1
 print(count_char_dict)
 
