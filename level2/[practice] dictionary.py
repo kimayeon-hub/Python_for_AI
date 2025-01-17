bf4821f49d1aca313e75e157dbbc1ac42cc77059
kimayeon-hub <ayun4953@gmail.com>
2025-01-17 14:01:19 +0900

    practice dictionary

--- level2/[practice] dictionary.py
+++ level2/[practice] dictionary.py
@@ -9,3 +9,31 @@ score_info['var'] = 100
 print(score_info)
 
 
+# scores의 정보들을 dictionary에 저장하기
+import random
+
+n_students = 10
+scores = [random.randint(0, 100) for _ in range(n_students)]
+print(f"{scores = }")
+
+score_info = {}
+
+score_info['mean'] = sum(scores) / len(scores)
+score_info['var'] = sum((score - score_info['mean'])**2 for score in scores) / len(scores)
+score_info['std'] = score_info['var']**0.5
+
+max_score, max_score_idx = None, None
+min_score, min_score_idx = None, None
+for score_idx, score in enumerate(scores):
+    if max_score == None or score > max_score:
+        max_score = score
+        max_score_idx = score_idx
+    elif min_score == None or score < min_score:
+        min_score = score
+        min_score_idx = score_idx
+score_info['max'] = max_score
+score_info['max_idx'] = max_score_idx
+score_info['min'] = min_score
+score_info['min_idx'] = min_score_idx
+
+print(score_info)
